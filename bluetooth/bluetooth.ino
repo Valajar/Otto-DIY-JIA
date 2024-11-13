ba0572c3aa5ac131e2a873e2283b9367c3e50c1b
Valajar <noah-brueggemann@outlook.de>
2024-11-13 10:34:02 +0100

    Update bluetooth.ino
    Änderungen:
    Tastatur kann nun befehle erteilen, wenn Kabelverbindung besteht
    Bluetooth Befehle sollen immernoch empfangbar sein

--- bluetooth/bluetooth.ino
+++ bluetooth/bluetooth.ino
@@ -1,4 +1,18 @@
 #include <SoftwareSerial.h>
+#include <Otto.h>
+Otto Otto;
+
+
+#define LeftLeg 2 
+#define RightLeg 3
+#define LeftFoot 4 
+#define RightFoot 5 
+#define Buzzer  13 
+#define DIN A3 // Data In pin
+#define CS A2  // Chip Select pin
+#define CLK A1 // Clock pin
+#define Orientation 1 // 8x8 LED Matrix orientation  Top = 1, Bottom = 2, Left = 3, Right = 4 
+
 
 // Definiere die Pins für SoftwareSerial
 #define BT_RX_PIN 10  // RX Pin (Empfang)
@@ -8,35 +22,55 @@
 SoftwareSerial BTSerial(BT_RX_PIN, BT_TX_PIN);
 
 void setup() {
+  Otto.init(LeftLeg, RightLeg, LeftFoot, RightFoot, true, Buzzer); //Set the servo pins and Buzzer pin
+  Otto.initMATRIX(DIN, CS, CLK, Orientation);
+  Otto.home();
+  delay(50);
+
+  
   // Beginne die serielle Kommunikation mit dem PC
   Serial.begin(9600);  // Standard-Serial für den Monitor
   // Beginne die serielle Kommunikation mit dem Bluetooth-Modul
   BTSerial.begin(9600);  // HC-05/HC-06 kommuniziert normalerweise mit 9600 Baud
-
-  Serial.println("Bluetooth is ready!");
+  Serial.println("Verbindung hergestellt. Bluetooth ist bereit und serielle Verbindung aktiv!");
 }
 
 void loop() {
   // Wenn Daten vom Bluetooth-Modul empfangen werden
   if (BTSerial.available()) {
     char command = BTSerial.read();  // Lese den empfangenen Befehl
-    Serial.print("Empfangener Befehl: ");
+    Serial.print("Bluetooth-Befehl empfangen: ");
     Serial.println(command);
-
-    // Verarbeite den Befehl und steuere Otto oder andere Aktionen
-    if (command == 'W') {
-      Serial.println("Vorwärts!");
-      // Hier kannst du Otto dazu anweisen, vorwärts zu gehen
-    } else if (command == 'S') {
-      Serial.println("Stop!");
-      // Hier kannst du Otto anweisen, zu stoppen
-    }
-    // Weitere Befehle können hier hinzugefügt werden...
+    processCommand(command);  // Verarbeite den Befehl
   }
 
-  // Wenn du auch vom PC senden möchtest:
+  // Wenn Daten vom seriellen Monitor (PC über USB) empfangen werden
   if (Serial.available()) {
     char pcCommand = Serial.read();
-    BTSerial.write(pcCommand);  // Sende den Befehl an das Bluetooth-Modul
+    Serial.print("USB-Befehl empfangen: ");
+    Serial.println(pcCommand);
+    processCommand(pcCommand);  // Verarbeite den Befehl
+  }
+}
+
+// Funktion zur Verarbeitung der Bewegungsbefehle
+void processCommand(char command) {
+  // Verarbeite den Befehl und steuere Otto entsprechend
+  if (command == 'w') {
+    Serial.println("Vorwärts!");
+    Otto.walk(5, 1000, 1);
+    // Funktion für Otto DIY vorwärts
+  } else if (command == 's') {
+    Serial.println("Rückwärts!");
+    Otto.walk(5, 1000, -1); 
+    // Funktion für Otto DIY rückwärts laufen
+  } else if (command == 'a') {
+    Serial.println("Links!");
+    Otto.turn(15, 1000, 1);
+    // Funktion für Otto DIY links drehen
+  } else if (command == 'd') {
+    Serial.println("Rechts!");
+    Otto.turn(15, 1000, -1);
+    // Funktion für Otto DIY rechts drehen
   }
 }
